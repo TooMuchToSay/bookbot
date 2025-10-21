f309a7ddd4474f9db8b05d0859cdab7418f6f260
toomuchtosay <tomuchtosay16@gmail.com>
2025-10-21 17:38:08 -0400

    H: refactored to split main.py to  only call in the functions of the data analysis from an import in the file stat s.py that actually as the word count file. Gonna work on the following character  counter after

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
+from stats import get_num_words
 def main():
     total_words = 0
     with open("/home/toomuchtosay/bookbot/books/frankenstein.txt") as f:
         file_contents = f.read()
-        word_count = get_word_count(file_contents)
+        word_count = get_num_words(file_contents, total_words)
         total_words += word_count
     print(f"Found {total_words} total words")
-def get_word_count(file):
-    #counted = len(file.split(' '))
-    stuff = len(file.split())
+#def get_word_count(file):
+ #   #counted = len(file.split(' '))
+  
+#stuff = len(file.split())
     #sentence = file.split(' ')
 
-    return stuff
+ #   return stuff
 
 main()
---
+++ stats.py
@@ -0,0 +1,10 @@
+def get_num_words(file, total):
+    count = len(file.split())
+    total += count
+    return total
+def total_characters(file, total):
+    #dictionary = {}
+    #for char in file:
+    #    if dictionary[char]
+#    dictionary[char] = 
+
