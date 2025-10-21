e9421a9e74b76b5ee187de99c3a10b96ee450141
toomuchtosay <tomuchtosay16@gmail.com>
2025-10-21 17:09:54 -0400

    G: made a simple two function program that calls each line and returns a total number of words in the file

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 def main():
+    total_words = 0
     with open("/home/toomuchtosay/bookbot/books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+        word_count = get_word_count(file_contents)
+        total_words += word_count
+    print(f"Found {total_words} total words")
+def get_word_count(file):
+    #counted = len(file.split(' '))
+    stuff = len(file.split())
+    #sentence = file.split(' ')
+
+    return stuff
 
 main()
