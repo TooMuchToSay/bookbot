527e8b5cff35c6811945da0201aaa0d33b54f029
toomuchtosay <tomuchtosay16@gmail.com>
2025-10-21 18:14:04 -0400

    Ha: added a function to count the characters in the file and present a dictionary of all those counts

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from stats import get_num_words
+from stats import total_characters 
 def main():
     total_words = 0
     with open("/home/toomuchtosay/bookbot/books/frankenstein.txt") as f:
         file_contents = f.read()
+        dictionary = total_characters(file_contents)
         word_count = get_num_words(file_contents, total_words)
         total_words += word_count
     print(f"Found {total_words} total words")
+    print(dictionary)
 #def get_word_count(file):
  #   #counted = len(file.split(' '))
   
--- stats.py
+++ stats.py
@@ -2,9 +2,16 @@ def get_num_words(file, total):
     count = len(file.split())
     total += count
     return total
-def total_characters(file, total):
-    #dictionary = {}
-    #for char in file:
-    #    if dictionary[char]
-#    dictionary[char] = 
-
+def total_characters(file):
+    dictionary = {}
+    for i in file.lower():
+        #if i not in dictionary.keys():
+         #   num = file.count(i.lower())
+           # dictionary[i] = num
+        #elif i in dictionary.keys():
+         #   pass
+        if i not in dictionary:
+            dictionary[i] = 1
+        else:
+            dictionary[i] = dictionary[i]+ 1
+    return dictionary
