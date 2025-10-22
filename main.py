1354423808433d3559c776ca2631794533a0368e
toomuchtosay <tomuchtosay16@gmail.com>
2025-10-22 16:53:03 -0400

    I: added the ability generate a list of each character, their number of items, and then a sorted list of those number of characters. Then outputs a nicely formatted report. Then rewrote it to take sys.argv arguments to any path to a txt file

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 from stats import get_num_words
-from stats import total_characters 
+from stats import total_characters
+from stats import generate_report
+import sys
 def main():
     total_words = 0
-    with open("/home/toomuchtosay/bookbot/books/frankenstein.txt") as f:
-        file_contents = f.read()
-        dictionary = total_characters(file_contents)
-        word_count = get_num_words(file_contents, total_words)
-        total_words += word_count
-    print(f"Found {total_words} total words")
-    print(dictionary)
+    try:
+        with open(f"{sys.argv[1]}") as f:
+            file_contents = f.read()                
+            dictionary = total_characters(file_contents)
+            word_count = get_num_words(file_contents, total_words)
+            total_words += word_count
+        print(f"============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...\n----------- Word Count ----------\nFound {total_words} total words\n--------- Character Count -------")
+        report = generate_report(dictionary)
+        for i in range(0, len(report)):
+            print(f"{report[i]['char'].strip()}: {report[i]['num']}")
+        print("============= END ===============")
+    except IndexError:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+                      
 #def get_word_count(file):
  #   #counted = len(file.split(' '))
   
--- stats.py
+++ stats.py
@@ -2,6 +2,7 @@ def get_num_words(file, total):
     count = len(file.split())
     total += count
     return total
+
 def total_characters(file):
     dictionary = {}
     for i in file.lower():
@@ -10,8 +11,24 @@ def total_characters(file):
            # dictionary[i] = num
         #elif i in dictionary.keys():
          #   pass
-        if i not in dictionary:
+        if i == ' ':
+            pass
+        elif i not in dictionary:
             dictionary[i] = 1
         else:
             dictionary[i] = dictionary[i]+ 1
+        
     return dictionary
+
+def sort_function(list):
+    return list['num']
+
+def generate_report(dictionary):
+    total_dicts = {}
+    all_dicts = []
+    for key in dictionary:
+        #print(key, dictionary[key])
+        new_dict = {'char': key} | {'num': dictionary[key]}
+        all_dicts.append(new_dict)
+    all_dicts.sort(reverse=True, key=sort_function)
+    return all_dicts
